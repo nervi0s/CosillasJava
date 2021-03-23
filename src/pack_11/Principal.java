1b445ed5bb4f9cec6747d53b98ea204cc6eae9a5
nervi0s <nervi0s@protonmail.com>
2021-03-23 23:22:49 +0100

    Hilos tipo Ping Pong

---
+++ src/pack_11/Principal.java
@@ -0,0 +1,43 @@
+package pack_11;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Object o = new Object();
+		ParImparHilo h1 = new ParImparHilo("Hilo Par", o);
+		ParImparHilo h2 = new ParImparHilo("Hilo Impar", o);
+
+		h1.start();
+		h2.start();
+
+	}
+
+}
+
+class ParImparHilo extends Thread {
+
+	private static Integer intObject = 0;
+	private Object o;
+
+	public ParImparHilo(String nombre, Object o) {
+		super(nombre);
+		this.o = o;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			synchronized (o) {
+				System.out.println(getName() + " -> " + intObject++);
+				try {
+					Thread.sleep(1000);
+					o.notifyAll();
+					o.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
