b7284260a2a46fd30244966014b628fd1c13bb92
nervi0s <nervi0s@protonmail.com>
2021-03-23 10:56:58 +0100

    Comentario sobre el método sincronizado

--- src/pack_09/Messenger.java
+++ src/pack_09/Messenger.java
@@ -7,4 +7,13 @@ public class Messenger {
 	synchronized public void sendMessage() {
 		System.out.println(msg + " desde " + Thread.currentThread().getName());
 	}
+	// Podríamos pensar que como hemos añadido la palabra synchronized delente del
+	// método (y por lo tanto, solo un hilo va a poder hacer uso de este método
+	// hasta que sea liberdo) que el mensaje por consola será alternante entre un
+	// hilo y otro, sin embargo, al tener un bucle while infinito en el método run
+	// de los hilos, estós están continuamente "compitiendo" por hacer uso del
+	// método sincronizado y puede darse la situación de que al liberar el método
+	// sincronizado para que lo use otro hilo, puede que el mismo hilo que lo liberó
+	// entre a usarlo de nuevo, de hecho es lo que ocurre como vemos en la salida de
+	// la consola.
 }
