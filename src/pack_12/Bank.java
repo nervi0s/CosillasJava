ff8a3741892090db8b92c18ab99ab42e1fd71b8f
nervi0s <nervi0s@protonmail.com>
2021-03-24 18:06:53 +0100

    Método para obtener cuenta asociada a un número

--- src/pack_12/Bank.java
+++ src/pack_12/Bank.java
@@ -7,16 +7,20 @@ public class Bank {
 	private static Bank instance = new Bank();
 	private HashMap<Integer, BankAccount> accountsNumbersAndBankAccounts;
 
+	// Constructor privado ya que usaremos un patrón Singleton
 	private Bank() {
 		accountsNumbersAndBankAccounts = new HashMap<>();
 		accountsNumbersAndBankAccounts.put(123456, new BankAccount(123456));
 	}
 
+	// Para obtener la instancia única
 	public static Bank getInstance() {
 		return instance;
 	}
 
-	public HashMap<Integer, BankAccount> getAccountsNumbersAndBankAccounts() {
-		return accountsNumbersAndBankAccounts;
+	// De un banco podemos obtener la cuenta bancaria asociada a un número de cuenta
+	public BankAccount getBankAccount(int accountsNumber) {
+		return accountsNumbersAndBankAccounts.get(accountsNumber);
 	}
+
 }
